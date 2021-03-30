cdc08022ed6e80bfe2b6d0be88fafa8219d34f79
noldor <47348171+Delonel123@users.noreply.github.com>
2021-03-30 14:19:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  $('.main__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows:true,
+    autoplay:true,
+    fade:true
+  });
+});
